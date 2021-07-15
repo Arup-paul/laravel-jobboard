c9e24ad4456859e6222a3b559bbf1d9ecf2babc1
Arup paul <aruppaulctg@gmail.com>
2021-07-15 18:19:37 +0600

    add new job position

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class ListingController extends Controller
@@ -64,5 +67,91 @@ class ListingController extends Controller
               ]);
 
          return redirect()->to($listing->apply_link);
+     }
+
+     public function create(){
+         return view('listings.create');
+     }
+     public function store(Request $request){
+
+         $validateArray = [
+             'title' => 'required',
+             'company' => 'required',
+             'logo'  => 'file|max:2048',
+             'location' => 'required',
+             'apply_link' => 'required|url',
+             'content' => 'required',
+             'payment_method_id' => 'required',
+         ];
+
+         if(Auth::check()){
+             $validateArray = array_merge($validateArray,[
+                 'email' => 'required|email|unique:users',
+                 'password' => 'required|confirmed|min:5',
+                 'name' => 'required'
+             ]);
+         }
+
+         $request->validate($validateArray);
+
+
+
+         $user = Auth::user();
+
+         if(!$user){
+             $user = User::create([
+                 'name' => $request->name,
+                 'email' => $request->email,
+                 'password' => Hash::make($request->password),
+             ]);
+
+             $user->createAsStripeCustomer();
+
+             Auth::login($user);
+
+         }
+
+
+         try {
+             $amount  = 9900;
+             if($request->filled('is_highlighted')){
+                 $amount += 1900;
+             }
+
+             $user->charge($amount,$request->payment_method_id);
+
+             $nd = new \ParsedownExtra();
+
+             $listing = $user->listing()
+                     ->create([
+                         'title' => $request->title,
+                         'slug'  => Str::slug($request->title) . '-' . rand(1111,9999),
+                         'company' => $request->company,
+                         'logo' => basename($request->file('logo')->store('public')),
+                         'location' => $request->location,
+                         'apply_link' => $request->apply_link,
+                         'content' => $nd->text($request->content),
+                         'is_highlighted' => $request->filled('is_highlighted'),
+                         'is_active' => true
+                     ]);
+
+             foreach (explode(',',$request->tags) as $requestTag) {
+                 $tag = Tag::firstOrCreate([
+                     'slug' => Str::slug(trim($requestTag))
+                 ],[
+                     'name' => ucwords(trim($requestTag))
+                 ]);
+
+                 $tag->listings()->attach($listing->id);
+             }
+
+             return redirect()->route('dashboard');
+
+
+         } catch (\Exception $e){
+               return redirect()->back()
+                     ->withErrors(['error' => $e->getMessage()]);
+         }
+
      }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/',  [ListingController::class,'index'])->name('listings.index');
-Route::get('/create',  [ListingController::class,'create'])->name('listings.create');
 
-
+Route::get('/new',  [ListingController::class,'create'])
+    ->name('listings.create');
+Route::post('/new',  [ListingController::class,'store'])
+    ->name('listings.store');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
@@ -27,4 +29,6 @@ Route::get('/{listing}',  [ListingController::class,'show'])
       ->name('listings.show');
 Route::get('/{listing}/apply',  [ListingController::class,'apply'])
           ->name('listings.apply');
+
+
 
