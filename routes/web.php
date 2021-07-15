372934616bdb6a95288a8453ac98e6eedd0c9f33
Arup paul <aruppaulctg@gmail.com>
2021-07-15 18:41:20 +0600

    edit dashboard

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,11 @@ Route::get('/new',  [ListingController::class,'create'])
     ->name('listings.create');
 Route::post('/new',  [ListingController::class,'store'])
     ->name('listings.store');
-Route::get('/dashboard', function () {
-    return view('dashboard');
+Route::get('/dashboard', function (\Illuminate\Http\Request $request) {
+    return view('dashboard', [
+        'listings' => $request->user()->listings,
+
+    ]);
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
